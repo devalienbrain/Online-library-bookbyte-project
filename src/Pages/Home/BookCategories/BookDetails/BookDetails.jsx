0928117badce14e190437f6ea22133bfe2a47779
Alien Brain <murad.dub@gmail.com>
2023-11-07 01:04:05 +0600

    borrowed books added to db successfully

--- src/Pages/Home/BookCategories/BookDetails/BookDetails.jsx
+++ src/Pages/Home/BookCategories/BookDetails/BookDetails.jsx
@@ -1,8 +1,10 @@
 import { Link, useLoaderData, useNavigate, useParams } from "react-router-dom";
 import Swal from "sweetalert2";
-import React, { useState } from "react"; // Import React and useState
+import React, { useContext, useState } from "react"; // Import React and useState
+import { AuthContext } from "../../../../Provider/AuthProvider";
 
 const BookDetails = () => {
+  const { user } = useContext(AuthContext);
   const books = useLoaderData();
   const { id } = useParams();
   const navigate = useNavigate();
@@ -18,6 +20,39 @@ const BookDetails = () => {
   };
   const closeModal = () => {
     setIsModalOpen(false);
+  };
+
+  // HANDLE BORROW BTN CLICKED
+  const handleBorrowedBook = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const userName = form.name.value;
+    const userEmail = form.email.value;
+    const date = form.date.value;
+
+    const liveDateToday = new Date();
+
+    const bookBorrowInfo = {
+      // service_id: _id,
+      image,
+      bookName: name,
+      category,
+      userName,
+      userEmail,
+      returnDate: date,
+      borrowedDate: liveDateToday,
+    };
+    console.log(bookBorrowInfo);
+    // Info send to db
+    fetch("http://localhost:5000/borrowedBooks", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(bookBorrowInfo),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   };
 
   return (
@@ -50,23 +85,66 @@ const BookDetails = () => {
         </div>
       </div>
 
-      {/* Modal */}
+      {/* MODAL PART */}
       {isModalOpen && (
         <div className="modal-container">
           <dialog
-            id="my_modal_5"
+            id="To_Borrow_A_Book"
             className="modal modal-bottom sm:modal-middle"
             open
           >
-            <div className="modal-box">
-              <h3 className="font-bold text-lg">Hello!</h3>
-              <p className="py-4">
-                Press ESC key or click the button below to close
-              </p>
-              <div className="modal-action">
-                <button onClick={closeModal} className="btn">
-                  Close
-                </button>
+            <div className="modal-box bg-green-300 shadow-lg text-white">
+              <h3 className="font-black text-lg text-red-700">
+                Please Fill Required Information First
+              </h3>
+              <form
+                onSubmit={handleBorrowedBook}
+                className="flex flex-col gap-4 px-5 py-9"
+              >
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Your name"
+                  defaultValue={user?.displayName}
+                  className="input input-bordered text-blue-600"
+                  required
+                />
+                <input
+                  type="email"
+                  name="email"
+                  defaultValue={user?.email}
+                  placeholder="email"
+                  className="input input-bordered text-blue-600"
+                  required
+                  // readOnly
+                />
+                <label
+                  htmlFor="date"
+                  className="text-red-800 font-semibold text-sm"
+                >
+                  Select Book Return Date:
+                </label>
+                <input
+                  type="date"
+                  name="date"
+                  className="input input-bordered text-red-600"
+                  required
+                />
+                <div className="form-control mt-6">
+                  <button className="btn bg-green-800 hover:bg-green-700 text-white">
+                    submit
+                  </button>
+                </div>
+              </form>
+              <div className="flex justify-center">
+                <div className="modal-action">
+                  <p
+                    onClick={closeModal}
+                    className="underline text-xs italic text-red-700 hover:text-red-600"
+                  >
+                    close modal
+                  </p>
+                </div>
               </div>
             </div>
           </dialog>
